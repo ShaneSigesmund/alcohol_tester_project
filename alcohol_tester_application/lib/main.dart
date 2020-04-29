052b74add0346594589e82d8d8a9cf78e29917c2
Shane Sigesmund <shanesigesmund1@gmail.com>
2020-04-29 14:03:54 -0400

    Updated theme colour

--- alcohol_tester_application/lib/main.dart
+++ alcohol_tester_application/lib/main.dart
@@ -1,41 +1,38 @@
 import 'package:flutter/material.dart';
 
+Map<int, Color> color =
+{
+50:Color.fromRGBO(59,59,59, .1),
+100:Color.fromRGBO(59,59,59, .2),
+200:Color.fromRGBO(59,59,59, .3),
+300:Color.fromRGBO(59,59,59, .4),
+400:Color.fromRGBO(59,59,59, .5),
+500:Color.fromRGBO(59,59,59, .6),
+600:Color.fromRGBO(59,59,59, .7),
+700:Color.fromRGBO(59,59,59, .8),
+800:Color.fromRGBO(59,59,59, .9),
+900:Color.fromRGBO(59,59,59, 1),
+};
 void main() => runApp(MyApp());
 
+MaterialColor colorCustom = MaterialColor(0xFF3B3B3B, color);
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Alcohol Tester Application',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+       
+        primarySwatch: colorCustom,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Alcohol Tester Application'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
